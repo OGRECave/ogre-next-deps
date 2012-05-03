05b388d007f510f60d620bcc055b33544dcca6e4
Holger Frydrych <h.frydrych@gmx.de>
2012-05-03 18:42:55 +0200

    Updated zziplib to 0.13.62

--- src/zziplib/zzip/_config.h
+++ src/zziplib/zzip/_config.h
@@ -13,7 +13,7 @@
 
 /* Define to 1 if you have the <byteswap.h> header file. */
 #ifndef ZZIP_HAVE_BYTESWAP_H 
-//#define ZZIP_HAVE_BYTESWAP_H  1 
+#define ZZIP_HAVE_BYTESWAP_H  1 
 #endif
 
 /* Define to 1 if you have the <direct.h> header file. */
@@ -198,7 +198,7 @@
 
 /* Version number of package */
 #ifndef ZZIP_VERSION 
-#define ZZIP_VERSION  "0.13.60" 
+#define ZZIP_VERSION  "0.13.62" 
 #endif
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most

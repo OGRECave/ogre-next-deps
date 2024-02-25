b0d041ddc9dce6e2aca5bde23cf54a09d42d85e6
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2024-02-25 18:54:10 -0300

    Fix FreeImageRe build errors on various archs

--- src/FreeImage/Source/OpenEXR/Iex/IexConfigInternal.h
+++ src/FreeImage/Source/OpenEXR/Iex/IexConfigInternal.h
@@ -6,6 +6,9 @@
 //
 // Dealing with FPEs
 //
-#define HAVE_UCONTEXT_H 1
-#define IEX_HAVE_CONTROL_REGISTER_SUPPORT 1
+
+// This is dealth by ogre-next-deps src/FreeImage/CMakeLists.txt
+
+// #define HAVE_UCONTEXT_H 1
+// #define IEX_HAVE_CONTROL_REGISTER_SUPPORT 1
 /* #undef IEX_HAVE_SIGCONTEXT_CONTROL_REGISTER_SUPPORT */
